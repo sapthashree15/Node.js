50a79c49ec6fa269968d03163e09099eab4c714c
sapthashree15 <96560683+sapthashree15@users.noreply.github.com>
2022-08-29 19:34:26 +0530

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -21,69 +21,48 @@ let users = [
         email:"joyalwhite@gamil.com",
         DOB:"21-03-1989",
     },
-
 ];
-
-
 router.get("/:email",(req,res)=>{
-
     const email = req.params.email;
-  
     let filtered_users = users.filter((user) => user.email === email);
     res.send(filtered_users);
-    
 })  
-
-
-
 router.get("/",(req,res)=>{
     res.send(users);
 });
-
-
 router.post("/new/",(req,res)=>{
     users.push({"firstName":req.query.firstName,"lastName":req.query.lastName,"email":req.query.email,"DOB":req.query.DOB});
     res.send("The user" + (' ')+ (req.query.firstName) + " Has been added!")
 });
-
-
 router.put("/:email", (req, res) => {
     const email = req.params.email;
-
     let filtered_users = users.filter((user) => user.email === email);
-
     if (filtered_users.length > 0) {
         let filtered_user = filtered_users[0];
-        console.log(typeof req.params);
-        console.log(req.params);
-        let filtered_params = JSON.parse(req.params);
-        
-        // console.log(req.params.keys);
-        Object.keys(filtered_params).forEach(param => {
-            filtered_user(param)=req.params(param);
-        });
-    
-
+        let DOB = req.query.DOB;
+        let firstName = req.query.firstName;
+        let lastName = req.query.lastName;
+        if(DOB) {
+            filtered_user.DOB = DOB
+        }
+        if(firstName) {
+            filtered_user.firstName = firstName
+        }
+        if(lastName) {
+            filtered_user.lastName = lastName
+        }
         users = users.filter((user) => user.email != email);
-        users.add(filtered_user);
-  
-        res.send(`User with the email  ${email} deleted.`);
+        users.push(filtered_user);
+        res.send(`User with the email  ${email} updated.`);
     }
-
     else{
         res.send("Unable to find user!");
     }
-
   });
-
-
 router.delete("/:email", (req, res) => {
     const email = req.params.email;
-  
     users = users.filter((user) => user.email != email);
-  
     res.send(`User with the email  ${email} deleted.`);
   });
 
-
-module.exports=router;
+  module.exports=router;
