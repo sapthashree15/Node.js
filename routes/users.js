af326fdb36ceacd5c7ab8baf2f84cc60b1d038c6
lavskillup <lavanyas@skillup.tech>
2022-08-29 14:26:29 +0530

    Added the new users.js to branch

---
+++ routes/users.js
@@ -0,0 +1,41 @@
+import express from "express";
+
+const router = express.Router();
+
+let users = [
+    {
+        firstName: "John",
+        lastName: "wick",
+        email:"johnwick@gamil.com",
+        DOB:"22-01-1990",
+    },
+    {
+        firstName: "John",
+        lastName: "smith",
+        email:"johnsmith@gamil.com",
+        DOB:"21-07-1983",
+    },
+    {
+        firstName: "Joyal",
+        lastName: "white",
+        email:"joyalwhite@gamil.com",
+        DOB:"21-03-1989",
+    },
+
+];
+
+
+router.get("/",(req,res)=>{
+    res.send(users);
+});
+
+
+router.post("/new/",(req,res)=>{
+    users.push({"firstName":req.query.firstName,"lastName":req.query.lastName,"email":req.query.email,"DOB":req.query.DOB});
+    res.send(req.query.firstName + " Has been added!")
+})
+
+
+
+
+export default router;
